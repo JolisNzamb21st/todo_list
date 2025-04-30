af95003b705fcf035a935d78f0ef09a0ba50d406
JolisNzamb21st <nzambimanafranck@gmail.com>
2025-04-30 04:18:35 -0400

    added the initial app  structure

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -1,21 +1,25 @@
 import React, { useState } from 'react';
 import './TodoList.css';
-
+//initialization of states
 const TodoList = () => {
-  
+  const [todos,setTodos]=useState([]); //the todos state will represent a list of to do items
+  const [headingInput, setHeadingInut] = useState(''); // headingInput represents the value entered by the user for adding a new to do item.
+  const [listInputs,setListInputs] = useState({}); //This state will hold the value of input fields for each todo item individually.
+
 
   return (
     <>
       <div className="todo-container">
-        <h1 className="title">My Todo List</h1>
+        <h1 className="title">My Learning Journey Tracker</h1>
+        <h3 className="subtitle">Add your lesson</h3>
         <div className="input-container">
           <input
             type="text"
             className="heading-input"
-            placeholder="Enter heading"
+            placeholder="Enter your next planed course"
             
           />
-          <button className="add-list-button">Add Heading</button>
+          <button className="add-list-button">Add</button>
         </div>
       </div>
       <div className="todo_main">
